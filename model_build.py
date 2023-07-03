50d8de0ca9e7f40bd5eee1f3ccaf97e197a13170
raizal18 <ramakrishnanmurugan6@gmail.com>
2023-07-03 13:32:12 +0530

    stable base initial from new pov

---
+++ model_build.py
@@ -0,0 +1,27 @@
+import warnings
+from typing import Any, ClassVar, Dict, List, Optional, Tuple, Type, TypeVar, Union
+
+import numpy as np
+import torch as th
+from gymnasium import spaces
+from torch.nn import functional as F
+import gymnasium
+
+from stable_baselines3.common.buffers import ReplayBuffer
+from stable_baselines3.common.off_policy_algorithm import OffPolicyAlgorithm
+from stable_baselines3.common.policies import BasePolicy
+from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback, Schedule
+from stable_baselines3.common.utils import get_linear_fn, get_parameters_by_name, polyak_update
+from stable_baselines3.dqn.policies import CnnPolicy, DQNPolicy, MlpPolicy, MultiInputPolicy, QNetwork
+from stable_baselines3 import PPO
+from stable_baselines3 import DQN
+
+env = gymnasium.make("CartPole-v1")
+
+env = gymnasium.envs.registry.keys()
+
+model = DQN("MlpPolicy", env, verbose=1)
+
+model.learn(total_timesteps=20000)
+
+model.predict()
