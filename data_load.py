af6b7e754228c88cfac84ecd6b98dca7b607e96c
raizal18 <ramakrishnanmurugan6@gmail.com>
2023-07-03 13:25:52 +0530

    stable_baseline_implementation

--- data_load.py
+++ data_load.py
@@ -1,9 +1,6 @@
 import os
 import pandas as pd
-import pandas_profiling
 import matplotlib.pyplot as plt
-
-
 
 DATA_PATH  = '.\\data'
 
@@ -11,24 +8,10 @@ available_files = os.listdir(DATA_PATH)
 
 csv_files = [file for file in available_files if file.endswith('.csv')]
 
+
 print('\033[1m' + 'Available Datasets :' + '\033[0m', '\n')
 
 for file in csv_files:
     print(file)
-
-commons_codec_raw = pd.read_csv(os.path.join(DATA_PATH, csv_files[0]))
-
-print(commons_codec_raw.info())
-
-
-iofrol_raw = pd.read_csv(os.path.join(DATA_PATH, csv_files[8]))
-
-print(iofrol_raw.info())
-
-
-# profile = pandas_profiling.ProfileReport(commons_codec_raw)
-
-# profile.to_file('report_commens_codec.html')
-
 
 
